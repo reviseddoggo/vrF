765ccd97ff4ac2288dc1fea6a2d0bab1e0437a03
reviseddoggo <109011377+reviseddoggo@users.noreply.github.com>
2022-09-05 19:28:06 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,14 +1,16 @@
-local vRaceFUCKER = Instance.new("ScreenGui")
-local OpenvRaceFUCKER = Instance.new("TextButton")
+
+local real = Instance.new("ScreenGui")
+local real_2 = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
 local UIGradient = Instance.new("UIGradient")
-local mainVicRaceFUCKER = Instance.new("Frame")
+local real_3 = Instance.new("Frame")
 local UICorner_2 = Instance.new("UICorner")
 local Frame = Instance.new("Frame")
 local lp = Instance.new("TextButton")
 local LocalPlayer = Instance.new("Frame")
 local TextBox = Instance.new("TextBox")
 local TextLabel = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
 local c = Instance.new("TextButton")
 local Credits = Instance.new("Frame")
 local TextLabel_2 = Instance.new("TextLabel")
@@ -24,45 +26,45 @@ local TextLabel_7 = Instance.new("TextLabel")
 
 --Properties:
 
-vRaceFUCKER.Name = "vRaceFUCKER"
-vRaceFUCKER.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-vRaceFUCKER.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+real.Name = "real."
+real.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+real.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-OpenvRaceFUCKER.Name = "OpenvRaceFUCKER"
-OpenvRaceFUCKER.Parent = vRaceFUCKER
-OpenvRaceFUCKER.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
-OpenvRaceFUCKER.Position = UDim2.new(0.0871710479, 0, 0.390057355, 0)
-OpenvRaceFUCKER.Size = UDim2.new(0, 200, 0, 50)
-OpenvRaceFUCKER.Font = Enum.Font.SourceSans
-OpenvRaceFUCKER.Text = "OPEN UI"
-OpenvRaceFUCKER.TextColor3 = Color3.fromRGB(255, 255, 255)
-OpenvRaceFUCKER.TextScaled = true
-OpenvRaceFUCKER.TextSize = 14.000
-OpenvRaceFUCKER.TextStrokeTransparency = 0.000
-OpenvRaceFUCKER.TextWrapped = true
+real_2.Name = "real."
+real_2.Parent = real
+real_2.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+real_2.Position = UDim2.new(0.0871710479, 0, 0.390057355, 0)
+real_2.Size = UDim2.new(0, 200, 0, 50)
+real_2.Font = Enum.Font.SourceSans
+real_2.Text = "OPEN UI"
+real_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+real_2.TextScaled = true
+real_2.TextSize = 14.000
+real_2.TextStrokeTransparency = 0.000
+real_2.TextWrapped = true
 
-UICorner.Parent = OpenvRaceFUCKER
+UICorner.Parent = real_2
 
 UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 0, 4)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
-UIGradient.Parent = OpenvRaceFUCKER
+UIGradient.Parent = real_2
 
-mainVicRaceFUCKER.Name = "mainVicRaceFUCKER"
-mainVicRaceFUCKER.Parent = vRaceFUCKER
-mainVicRaceFUCKER.Active = true
-mainVicRaceFUCKER.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
-mainVicRaceFUCKER.Position = UDim2.new(0.336348683, 0, 0.337864906, 0)
-mainVicRaceFUCKER.Size = UDim2.new(0, 424, 0, 305)
-mainVicRaceFUCKER.Visible = false
+real_3.Name = "real."
+real_3.Parent = real
+real_3.Active = true
+real_3.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
+real_3.Position = UDim2.new(0.336348683, 0, 0.337864906, 0)
+real_3.Size = UDim2.new(0, 424, 0, 305)
+real_3.Visible = false
 
-UICorner_2.Parent = mainVicRaceFUCKER
+UICorner_2.Parent = real_3
 
-Frame.Parent = mainVicRaceFUCKER
+Frame.Parent = real_3
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Frame.Position = UDim2.new(0.22169812, 0, 0, 0)
 Frame.Size = UDim2.new(0, 8, 0, 305)
 
 lp.Name = "lp"
-lp.Parent = mainVicRaceFUCKER
+lp.Parent = real_3
 lp.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
 lp.Position = UDim2.new(0.0141509436, 0, 0.226229519, 0)
 lp.Size = UDim2.new(0, 82, 0, 50)
@@ -89,8 +91,10 @@ TextBox.Font = Enum.Font.SourceSans
 TextBox.PlaceholderColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.PlaceholderText = "Type Walkspeed here"
 TextBox.Text = ""
-TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.TextScaled = true
 TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
 
 TextLabel.Parent = LocalPlayer
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -105,8 +109,20 @@ TextLabel.TextSize = 14.000
 TextLabel.TextStrokeTransparency = 0.000
 TextLabel.TextWrapped = true
 
+TextButton.Parent = LocalPlayer
+TextButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+TextButton.Position = UDim2.new(0.357142866, 0, 0.226229504, 0)
+TextButton.Size = UDim2.new(0, 200, 0, 50)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "CHANGE WALKSPEED"
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextStrokeTransparency = 0.000
+TextButton.TextWrapped = true
+
 c.Name = "c"
-c.Parent = mainVicRaceFUCKER
+c.Parent = real_3
 c.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
 c.Position = UDim2.new(0.0141509436, 0, 0.422950834, 0)
 c.Size = UDim2.new(0, 82, 0, 50)
@@ -166,7 +182,7 @@ TextLabel_4.TextStrokeTransparency = 0.000
 TextLabel_4.TextWrapped = true
 
 h.Name = "h"
-h.Parent = mainVicRaceFUCKER
+h.Parent = real_3
 h.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
 h.Position = UDim2.new(0.0141509436, 0, 0.0327869058, 0)
 h.Size = UDim2.new(0, 82, 0, 50)
@@ -211,7 +227,7 @@ TextLabel_6.TextSize = 14.000
 TextLabel_6.TextWrapped = true
 
 m.Name = "m"
-m.Parent = mainVicRaceFUCKER
+m.Parent = real_3
 m.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
 m.Position = UDim2.new(0.0141509436, 0, 0.626229525, 0)
 m.Size = UDim2.new(0, 82, 0, 50)
@@ -246,8 +262,8 @@ TextLabel_7.TextWrapped = true
 
 -- Scripts:
 
-local function MQAG_fake_script() -- OpenvRaceFUCKER.LocalScript 
-	local script = Instance.new('LocalScript', OpenvRaceFUCKER)
+local function EPNE_fake_script() -- real_2.LocalScript 
+	local script = Instance.new('LocalScript', real_2)
 
 	local count = 1
 	script.Parent.MouseButton1Click:Connect(function()
@@ -266,21 +282,22 @@ local function MQAG_fake_script() -- OpenvRaceFUCKER.LocalScript
 			end
 		end
 		task.wait(.1)
-		script.Parent.Parent.mainVicRaceFUCKER.Visible = true
+		script.Parent.Parent["real."].Visible = true
 		task.wait(.1)
 		script.Parent:Destroy()
 	end)
 end
-coroutine.wrap(MQAG_fake_script)()
-local function TRFEWTR_fake_script() -- TextBox.LocalScript 
-	local script = Instance.new('LocalScript', TextBox)
+coroutine.wrap(EPNE_fake_script)()
+local function IZRCRYF_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
 
-	while true do
-		game.Players.LocalPlayer:WaitForChild("Character"):WaitForChild("Humanoid").Walkspeed = script.Parent.Text
-	end
+	local plr = game.Players.LocalPlayer
+	script.Parent.MouseButton1Click:Connect(function()
+		plr.Character.Humanoid.WalkSpeed = script.Parent.Parent.TextBox.Text	
+	end)
 end
-coroutine.wrap(TRFEWTR_fake_script)()
-local function MFKDC_fake_script() -- lp.LocalScript 
+coroutine.wrap(IZRCRYF_fake_script)()
+local function MUIHOFY_fake_script() -- lp.LocalScript 
 	local script = Instance.new('LocalScript', lp)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -290,8 +307,8 @@ local function MFKDC_fake_script() -- lp.LocalScript
 		script.Parent.Parent.m.extra.Visible = false
 	end)
 end
-coroutine.wrap(MFKDC_fake_script)()
-local function OGCBIK_fake_script() -- c.LocalScript 
+coroutine.wrap(MUIHOFY_fake_script)()
+local function JUQDS_fake_script() -- c.LocalScript 
 	local script = Instance.new('LocalScript', c)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -301,20 +318,20 @@ local function OGCBIK_fake_script() -- c.LocalScript
 		script.Parent.Parent.m.extra.Visible = false
 	end)
 end
-coroutine.wrap(OGCBIK_fake_script)()
-local function OTLGO_fake_script() -- TextLabel_5.LocalScript 
+coroutine.wrap(JUQDS_fake_script)()
+local function XYFN_fake_script() -- TextLabel_5.LocalScript 
 	local script = Instance.new('LocalScript', TextLabel_5)
 
 	script.Parent.Text = "Welcome, "..game.Players.LocalPlayer.Name.."!"
 end
-coroutine.wrap(OTLGO_fake_script)()
-local function SPQW_fake_script() -- TextLabel_6.LocalScript 
+coroutine.wrap(XYFN_fake_script)()
+local function XLCWK_fake_script() -- TextLabel_6.LocalScript 
 	local script = Instance.new('LocalScript', TextLabel_6)
 
 	script.Parent.Text = "Welcome, "..game.Players.LocalPlayer.Name.."!"
 end
-coroutine.wrap(SPQW_fake_script)()
-local function NYEUV_fake_script() -- h.LocalScript 
+coroutine.wrap(XLCWK_fake_script)()
+local function GXQU_fake_script() -- h.LocalScript 
 	local script = Instance.new('LocalScript', h)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -324,8 +341,8 @@ local function NYEUV_fake_script() -- h.LocalScript
 		script.Parent.Parent.m.extra.Visible = false
 	end)
 end
-coroutine.wrap(NYEUV_fake_script)()
-local function RWSLW_fake_script() -- m.LocalScript 
+coroutine.wrap(GXQU_fake_script)()
+local function FWMAYEP_fake_script() -- m.LocalScript 
 	local script = Instance.new('LocalScript', m)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -335,10 +352,10 @@ local function RWSLW_fake_script() -- m.LocalScript
 		script.Parent.Parent.c.Credits.Visible = false
 	end)
 end
-coroutine.wrap(RWSLW_fake_script)()
-local function IBCDOI_fake_script() -- mainVicRaceFUCKER.LocalScript 
-	local script = Instance.new('LocalScript', mainVicRaceFUCKER)
+coroutine.wrap(FWMAYEP_fake_script)()
+local function HRNYFIG_fake_script() -- real_3.LocalScript 
+	local script = Instance.new('LocalScript', real_3)
 
 	script.Parent.Draggable = true
 end
-coroutine.wrap(IBCDOI_fake_script)()
+coroutine.wrap(HRNYFIG_fake_script)()
